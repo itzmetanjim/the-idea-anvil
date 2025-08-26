29b06542007a1b8fa471688650e2f4ae6bf4d2e2
Itz me Tanjim <tanjimkamal1@gmail.com>
2025-08-26 18:42:33 +0600

    Init

---
+++ src/main/java/org/tanjimkamal/the_idea_anvil/The_idea_anvil.java
@@ -0,0 +1,10 @@
+package org.tanjimkamal.the_idea_anvil;
+
+import net.fabricmc.api.ModInitializer;
+
+public class The_idea_anvil implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/org/tanjimkamal/the_idea_anvil/client/The_idea_anvilClient.java
@@ -0,0 +1,10 @@
+package org.tanjimkamal.the_idea_anvil.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class The_idea_anvilClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
